ae6f4fb0d339494a34b7286d1f36862d952ba96a
KyokaUkai <ukai.kyoka.1015@gmail.com>
2023-12-25 23:28:49 +0900

    投稿詳細画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,25 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
- 
         <title>Restaurant</title>
-
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-      
-      
-        </head>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
     <body class="antialiased">
-      <h1>イタリアのおいしいレストラン</h1>
-          <div class='posts'>
-              @foreach ($posts as $post)
-           <div class='post'>
-               <h2 class='restaurant'>{{$post->restaurant}}</h2>
-               <p class='body'>{{ $post->body }}</p>
-           </div>
-              @endforeach
-          </div>
+        <h1>イタリアのおいしいレストラン</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <a href="/posts/{{ $post->id }}"><h2 class='restaurant'>{{ $post->restaurant }}</h2></a>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
